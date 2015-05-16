d0105d4f04d4a09d59b9bf1ebb446ee9a59b4ce3
Kristaps_B <kristaps_bbks@inbox.lv>
2015-05-16 23:04:56 +0300

    Komentāru kļūda!

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
     belongs_to :user
     belongs_to :book
     
-    validates :comment, :presence => true
+    validates :txt, :presence => true
     
     RATINGS = [1, 2, 3, 4, 5]
 end
